5f6971c2386ad08fc3caa1572caff13bb139293d
jiachen <jchen@cheweilai.cn>
2018-03-16 11:45:07 +0800

    update_doc

--- oauth_doc/api_project.js
+++ oauth_doc/api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2018-03-16T03:41:21.489Z",
+    "time": "2018-03-16T03:45:06.535Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
